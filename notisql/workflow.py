98131e675861b66235fd3b0bb518bbfb90b5f006
HANBYUL <kyonggi@kakao.com>
2023-03-21 08:27:39 +0900

    Add code to test 'getenv' in actions (#3)

--- notisql/workflow.py
+++ notisql/workflow.py
@@ -1,2 +1,16 @@
 # TEST FILE OF GITHUB WORKFLOW
-print("TEST FILE OF GITHUB WORKFLOW")
+# print("TEST FILE OF GITHUB WORKFLOW")
+
+import os
+from dotenv import load_dotenv
+from Notisql import version
+
+# Load .env vaiables
+load_dotenv()
+
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+DATABASE_TOKEN = os.getenv("DATABASE_TOKEN")
+PAGE_TOKEN = os.getenv("PAGE_TOKEN")
+
+# Print version
+version()
