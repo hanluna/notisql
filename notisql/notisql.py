af3013d702483049731a85c2f8e97c1a88274a40
HANBYUL <kyonggi@kakao.com>
2023-03-20 16:27:05 +0900

    Rename repository & project name notapy to notisql

--- notisql/notisql.py
+++ notisql/notisql.py
@@ -1,15 +1,19 @@
 import os
 import notion_client
 from termcolor import colored
+from .page import PageInformation
 
 # Prevent error when running on windows
 if os.name == "nt": os.system("color")
 
 
-class Notapy:
+class Notisql:
     def __init__(self, api_key: str):
         self.client = notion_client.Client(auth=api_key)
 
     def version(self):
         version = "0.0.1"
-        print(colored(f"[-] Notapy Version: {version}", "green"))
+        print(colored(f"[-] Notisql Version: {version}", "green"))
+
+    def page_information(self, database_id: str):
+        return PageInformation(client=self.client, database_id=database_id)
